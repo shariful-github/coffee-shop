5ad9a287a58cda3c0c8ca0313449ee52735f45f4
sharif-github <107545661+sharif-github@users.noreply.github.com>
2024-02-05 23:19:11 +0600

    Add Coffee button added

--- coffee-store-client/src/App.jsx
+++ coffee-store-client/src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import { useLoaderData } from 'react-router-dom'
+import { Link, useLoaderData } from 'react-router-dom'
 import CoffeeCard from './components/CoffeeCard';
 import { useState } from 'react';
 
@@ -11,6 +11,11 @@ function App() {
   return (
     <>
       <h1 className='text-teal-600 text-5xl text-center font-bold my-10'>Coffee Shop</h1>
+      <div className='text-right'>
+        <Link to={'http://localhost:5173/addcoffee'}>
+          <button className="btn btn-info mr-5">Add Coffee</button>
+        </Link>
+      </div>
       <div className='w-3/4 mx-auto grid grid-cols-2 gap-5 mb-10'>
         {
           coffees.map(coffee =>
@@ -19,7 +24,7 @@ function App() {
               coffee={coffee}
               coffees={coffees}
               setCoffees={setCoffees}
-            ></CoffeeCard>  
+            ></CoffeeCard>
           )
         }
       </div>
